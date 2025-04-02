0ad9b41712e65f590e9c5c280653f9cfd577b3ab
Marwane OUKACHA <oukachamarwane0@gmail.com>
2025-04-02 18:31:46 +0000

    first commit

---
+++ src/components/BookForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import CheckIn from '../components/CheckIn';
+import KidsDropdown from '../components/KidsDropdown';
+import AdultsDropdown from '../components/AdultsDropdown';
+import CheckOut from '../components/CheckOut';
+
+
+
+
+const BookForm = () => {
+  return <form className='h-[300px] bg-green-100 w-full lg:h-[70px]'>
+    <div className='flex flex-col w-full h-full lg:flex-row'>
+      <div className='flex-1 border-r '>
+        <CheckIn />
+      </div>
+      <div className='flex-1 border-r '>
+        <CheckOut/>
+      </div>
+      <div className='flex-1 border-r '>
+        <AdultsDropdown/>
+      </div>
+      <div className='flex-1 border-r '>
+        <KidsDropdown/>
+      </div>
+    </div>
+  </form>;
+};
+
+export default BookForm;
---
+++ src/components/CheckIn.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import '../datepicker.css';
+import {BsCalendar} from 'react-icons/bs'
+
+
+const CheckIn = () => {
+  return <div>CheckIn</div>;
+};
+
+export default CheckIn;
